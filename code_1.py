33194d00726df4a3fbdebdce9e378ebea60b39f5
namratanwani <tanwaninamrata11@gmail.com>
2024-03-06 13:19:31 +0000

    fibonacci added

--- code_1.py
+++ code_1.py
@@ -1,5 +1,16 @@
 def square(n):
     return n**2
 
+
+def fibonacci(n):
+    lst = [0,1]
+    for i in range(n-2):
+        s = lst[-1] + lst[-2]
+        lst.append(s)
+    return lst
+
 result = square(4)
 print(result)
+
+fib_list = fibonacci(10)
+print(fib_list)
